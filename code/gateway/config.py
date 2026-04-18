af12f58bea8ff200d2d7ae9c021f8f1bb6dce153
Manohar Shetty <maddy98989@gmail.com>
2026-04-18 10:43:43 +0530

    Update config.py

--- code/gateway/config.py
+++ code/gateway/config.py
@@ -8,5 +8,6 @@ WORD2PDF_SERVICE = os.getenv("WORD2PDF_SERVICE_URL", "http://word2pdf-service:50
 USER_SERVICE = os.getenv("USER_SERVICE_URL", "http://user-service:5006")
 URL_SHORTENER_SERVICE = os.getenv("URL_SHORTENER_SERVICE_URL","http://url-shortner-service:50007")
 TODO_SERVICE = os.getenv("TODO_SERVICE_URL","http://todo:50009")
-AUDIO_COMBINER_SERVICE = os.getenv("AUDIO_COMBINER_SERVICE_URL","http://audio-combiner-service:5011")
 AUDIO_CUTTER_SERVICE = os.getenv("AUDIO_CUTTER_SERVICE_URL","http://audio-cutter-service:50010")
+AUDIO_COMBINER_SERVICE = os.getenv("AUDIO_COMBINER_SERVICE_URL","http://audio-combiner-service:5011")
+CIDR_SERVICE = os.getenv("CIDR_SERVICE_URL", "http://cidr-service:5012")

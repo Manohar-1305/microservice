df13e0532907d7228df04aa1cca9b4671017b829
Manohar Shetty <maddy98989@gmail.com>
2026-05-09 16:32:49 +0530

    Fix AUDIO_SERVICE_URL typo in config.py

--- code/gateway/config.py
+++ code/gateway/config.py
@@ -2,7 +2,7 @@ import os
 
 PDF_SERVICE = os.getenv("PDF_SERVICE_URL", "http://pdf-service:5001")
 YTDL_SERVICE = os.getenv("YTDL_SERVICE_URL", "http://youtube-service:5002")
-AUDIO_SERVICE = os.getenv("AUDIO_SERVICE_URL", "http://audio-conatainer-service:5003")
+AUDIO_SERVICE = os.getenv("AUDIO_SERVICE_URL", "http://audio-converter-service:5003")
 MUSIC_SERVICE = os.getenv("MUSIC_SERVICE_URL", "http://music-service:5004")
 WORD2PDF_SERVICE = os.getenv("WORD2PDF_SERVICE_URL", "http://word2pdf-service:5005")
 USER_SERVICE = os.getenv("USER_SERVICE_URL", "http://user-service:5006")
